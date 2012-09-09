50aae555899005691028982b266eec9384f67c79
Thoronador <thoronador@users.sourceforge.net>
2012-09-09 07:49:00 +0200

    add: function to get home directory of current user

--- common/DirectoryFunctions.cpp
+++ common/DirectoryFunctions.cpp
@@ -1,20 +1,20 @@
 /*
  -------------------------------------------------------------------------------
-    This file is part of the Morrowind Tools Project.
-    Copyright (C) 2011 Thoronador
+    This file is part of the Thoronador's common code library.
+    Copyright (C) 2011, 2012  Thoronador
 
-    The Morrowind Tools are free software: you can redistribute them and/or
-    modify them under the terms of the GNU General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    The Morrowind Tools are distributed in the hope that they will be useful,
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with the Morrowind Tools.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  -------------------------------------------------------------------------------
 */
 
@@ -23,6 +23,9 @@
 
 #if defined(_WIN32)
   #include <Windows.h>
+#elif defined(__linux__) || defined(linux)
+  #include <unistd.h>
+  #include <pwd.h>
 #endif
 
 bool directoryExists(const std::string& dirName)
@@ -52,7 +55,7 @@ bool createDirectory(const std::string& dirName)
 
 bool createDirectoryRecursive(const std::string& dirName)
 {
-  const std::string::size_type delimPos = dirName.rfind(MWTP::pathDelimiter);
+  const std::string::size_type delimPos = dirName.rfind(Thoro::pathDelimiter);
   if (delimPos==std::string::npos) return createDirectory(dirName);
 
   if (directoryExists(dirName.substr(0, delimPos)))
@@ -68,4 +71,30 @@ bool createDirectoryRecursive(const std::string& dirName)
   }
   //creation of parent directory failed
   return false;
+}
+
+bool getHomeDirectory(std::string& result)
+{
+  #if defined(_WIN32)
+    #warning Not implemented yet!
+    return false;
+  #elif defined(__linux__) || defined(linux)
+    const long int buf_size = sysconf(_SC_GETPW_R_SIZE_MAX);
+    char * buffer = new char[buf_size];
+    struct passwd info;
+    struct passwd * pwd_ptr = NULL;
+    int error = getpwuid_r(getuid(), &info, buffer, buf_size, &pwd_ptr);
+    if (error!=0)
+    {
+      //getpwuid_r() failed
+      delete [] buffer;
+      return false;
+    }
+    //success
+    result = std::string(info.pw_dir);
+    delete [] buffer;
+    return true;
+  #else
+    #error Unknown operating system!
+  #endif
 }
