770c33b1464177dd54a36a940dfbd1a70947b699
Thoronador <thoronador@users.sourceforge.net>
2015-04-11 14:33:54 +0200

    add encoding function from pmdb (uses libxml2)

---
+++ encoding/libxml2.cpp
@@ -0,0 +1,68 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the Private Message Database.
+    Copyright (C) 2015  Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include "libxml2.hpp"
+#include <cstring>
+#include <iostream>
+#include <libxml/encoding.h>
+
+namespace libthoro
+{
+
+namespace encoding
+{
+
+bool libxml2::UTF8_to_ISO_8859_1(const std::string& strUTF8, std::string& strISO)
+{
+  //maximum output size should not be more than length of UTF string (+ NUL)
+  int outLen = strUTF8.size();
+  unsigned char * outBuff = new unsigned char[outLen+1];
+  memset(outBuff, '\0', outLen+1);
+
+  const unsigned char * inPseudoBuff = reinterpret_cast<const unsigned char*>(strUTF8.c_str());
+  int inLen = strUTF8.size();
+  const int ret = UTF8Toisolat1(outBuff, &outLen, inPseudoBuff, &inLen);
+  if (ret <= 0)
+  {
+    //conversion failed
+    delete[] outBuff;
+    return false;
+  }
+
+  if (ret > strUTF8.size())
+  {
+    //seems strange, would be more than buffer can hold
+    delete[] outBuff;
+    return false;
+  }
+
+  strISO = std::string(reinterpret_cast<const char*>(outBuff));
+  delete[] outBuff;
+  if (strISO.size() < ret)
+  {
+    std::cout << "Encoding error: got only " << strISO.size() << " chars, but expected " << ret << " chars.\n";
+    return false;
+  }
+  return true;
+}
+
+} //namespace encoding
+
+} //namespace libthoro
---
+++ encoding/libxml2.hpp
@@ -0,0 +1,56 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the Private Message Database.
+    Copyright (C) 2015  Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#ifndef LIBTHORO_ENCODING_LIBXML2_HPP
+#define LIBTHORO_ENCODING_LIBXML2_HPP
+
+#include <string>
+
+namespace libthoro
+{
+
+namespace encoding
+{
+
+/** \brief class that uses libxml2's encoding capabilities
+ *
+ * \remarks Since libxml2 implementations usually make use of iconv internally,
+ *          you might as well use the string conversion functions provided in
+ *          the header StringConversion.hpp, which directly uses iconv.
+ */
+class libxml2
+{
+  public:
+    /** \brief converts an UTF-8-encoded string to an ISO-8859-1 encoded string
+     *
+     * \param strUTF8  input string, UTF8-encoded
+     * \param strISO   used to return the output string, ISO-88559-1-encoded
+     * \return Returns true in case of success, false in case of error.
+     *
+     */
+
+    static bool UTF8_to_ISO_8859_1(const std::string& strUTF8, std::string& strISO);
+}; //class
+
+} //namespace encoding
+
+} //namesoace libthoro
+
+#endif // LIBTHORO_ENCODING_LIBXML2_HPP
